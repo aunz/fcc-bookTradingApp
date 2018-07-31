d4c024b5b36223e25863651d25c9585d6dbd1fc2
aunz <aunz10@yahoo.com>
2018-07-31 19:14:34 +1200

    refact reqsByUser component

--- src/client/components/MyRequest.js
+++ src/client/components/MyRequest.js
@@ -91,52 +91,13 @@ export default class MyRequest extends PureComponent {
                         key={book.id}
                         gid={book.gid}
                       >
-                        {d => (
-                          <Query
-                            query={GET_USER_DETAIL}
-                            variables={{ id: book.rid }}
-                          >
-                            {({ data: { getUserDetail }, loading: loading2, error: error2 }) => {
-                              if (error2 && this.state.showError) return ErrorButton({
-                                onClick: () => { this.setState({ showError: false }) },
-                                children: 'Oops something went wrong!'
-                              })
-                              if (loading2) return <span className="m1">{spinner}</span>
-
-                              const status = book.status === null
-                                ? <Trade user={user} book={book} />
-                                : <div className="my-auto">You {book.status ? 'accepted' : 'declined'} this trade</div>
-
-                              return (
-                                <div className="flex m1 my2">
-                                  <button
-                                    className={buttonFlatClass + ' flex justify-center'}
-                                    style={{ width: '3rem' }}
-                                    type="button"
-                                    onClick={() => { this.setState({ selectedBook: d }) }}
-                                  >
-                                    <img
-                                      style={{ maxWidth: '3rem', maxHeight: '3rem' }}
-                                      src={d.smallThumbnail}
-                                      alt=""
-                                    />
-                                  </button>
-                                  <div className="flex flex-column mx1">
-                                    <span>{d.title}</span>
-                                    <small className="silver my-auto">Requested on {new Date(book.ts * 1000).toDateString()}</small>
-                                  </div>
-                                  <div
-                                    className="flex flex-column mx1 px1 center "
-                                    style={{ borderLeft: '1px solid silver', borderRight: '1px solid silver' }}
-                                  >
-                                    <span>Requested by</span>
-                                    <span className="my-auto">{getUserDetail && getUserDetail.name}</span>
-                                  </div>
-                                  {status}
-                                </div>
-                              )
-                            }}
-                          </Query>
+                        {gbook => (
+                          <TradingBook
+                            book={book}
+                            user={user}
+                            gbook={gbook}
+                            selectBook={b => { this.setState({ selectedBook: b }) }}
+                          />
                         )}
                       </GBook>
                     )
@@ -154,6 +115,73 @@ export default class MyRequest extends PureComponent {
           />
         )}
       </div>
+    )
+  }
+}
+
+
+class TradingBook extends PureComponent {
+  static propTypes = {
+    // gbook contains google book info, it's an extension to book
+    gbook: PropTypes.object, // eslint-disable-line
+    book: PropTypes.object, // eslint-disable-line
+    user: PropTypes.object, // eslint-disable-line
+    selectBook: PropTypes.func,
+  }
+  state = {
+    showError: true
+  }
+  render() {
+    const { gbook, book, user } = this.props
+    return (
+      <Query
+        query={GET_USER_DETAIL}
+        variables={{ id: book.rid }}
+      >
+        {({ data: { getUserDetail }, loading, error }) => {
+          if (loading || (error && this.state.showError)) return (
+            <EL
+              error={error}
+              loading={loading}
+              showError={this.state.showError}
+              onClick={() => { this.setState({ showError: false }) }}
+            />
+          )
+
+          const status = book.status === null
+            ? <Trade user={user} book={book} />
+            : <div className="my-auto">You {book.status ? 'accepted' : 'declined'} this trade</div>
+
+          return (
+            <div className="flex m1 my2">
+              <button
+                className={buttonFlatClass + ' flex justify-center'}
+                style={{ width: '3rem' }}
+                type="button"
+                onClick={() => { this.props.selectBook(gbook) }}
+              >
+                <img
+                  style={{ maxWidth: '3rem', maxHeight: '3rem' }}
+                  src={gbook.smallThumbnail}
+                  alt=""
+                />
+              </button>
+              <div className="flex flex-column mx1">
+                <span>{gbook.title}</span>
+                <small className="silver my-auto">Requested on {new Date(book.ts * 1000).toDateString()}</small>
+              </div>
+              <div
+                className="flex flex-column mx1 px1 center "
+                style={{ borderLeft: '1px solid silver', borderRight: '1px solid silver' }}
+              >
+                <span>Requested by</span>
+                <span className="my-auto">{getUserDetail && getUserDetail.name}</span>
+              </div>
+              {status}
+            </div>
+          )
+        }}
+      </Query>
     )
   }
 }
@@ -178,39 +206,57 @@ class Trade extends PureComponent {
         ]}
       >
         {(mutate, { loading, error }) => {
-          if (error && this.state.showError) return ErrorButton({
-            onClick: () => { this.setState({ showError: false }) },
-            children: 'Oops something went wrong!'
-          })
-          if (loading) return <span className="m1">{spinner}</span>
-          const className = buttonClass + ' mx1 '
-          const variables = { token: user.token, bid: book.bid, rid: book.rid }
+          if (loading || (error && this.state.showError)) return (
+            <EL
+              error={error}
+              loading={loading}
+              showError={this.state.showError}
+              onClick={() => { this.setState({ showError: false }) }}
+            />
+          )
+          const button = (type, text) => (
+            <button
+              className={buttonClass + ' mx1 '}
+              type="button"
+              onClick={() => {
+                const variables = { token: user.token, bid: book.bid, rid: book.rid, type }
+                this.setState({ showError: true })
+                mutate({ variables })
+              }}
+            >
+              {text}
+            </button>
+          )
           return (
             <Fragment>
-              <button
-                className={className}
-                type="button"
-                onClick={() => {
-                  variables.type = 'accept'
-                  mutate({ variables })
-                }}
-              >
-                Accept
-              </button>
-              <button
-                className={className + ' border-red '}
-                type="button"
-                onClick={() => {
-                  variables.type = 'decline'
-                  mutate({ variables })
-                }}
-              >
-                Decline
-              </button>
+              {button('accept', 'Accept')}
+              {button('decline', 'Decline')}
             </Fragment>
           )
         }}
       </Mutation>
     )
   }
+}
+
+function EL({ loading, error, showError, onClick, children }) { // show Error or Loading
+  if (loading) return <span className="m1">{spinner}</span>
+  if (error && showError) return (
+    <button
+      className={buttonFlatClass + ' red icon-cancel'}
+      onClick={onClick}
+      type="button"
+    >
+      {children || 'Oops something went wrong!'}
+    </button>
+  )
+  return null
+}
+
+EL.propTypes = {
+  onClick: PropTypes.func,
+  children: PropTypes.element,
+  loading: PropTypes.bool,
+  error: PropTypes.object, // eslint-disable-line
+  showError: PropTypes.bool,
 }
